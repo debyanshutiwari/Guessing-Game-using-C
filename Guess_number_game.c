427884d015ae7913f949c761ed958b73e732b49d
Debyanshu Tiwari <88588383+debyanshutiwari@users.noreply.github.com>
2021-10-15 19:38:23 +0530

    Update Guess_number_game.c

--- Guess_number_game.c
+++ Guess_number_game.c
@@ -8,7 +8,7 @@ int main()
     srand(time(0));
    number=rand()%100+1;
   
-   //kepp reunning the loop untill gussesd no. is correct
+   
    do{
        printf("Guess the number between 1 to 100\n");
        scanf("%d\n",&guess);
@@ -24,6 +24,6 @@ int main()
            printf("You guessed in %d attempt\n");
        }
        nguesses++;
-   }while(guess!=number); // while loop execute till guess no. will not equal number
+   }while(guess!=number); 
     return 0;
 }
